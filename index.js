fca2b2d485d5420f7c7db2bcef9da5d9078796a0
Mellon Obada <mellobadah123@gmail.com>
2025-03-17 21:57:31 +0300

    destructuring

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-const farmAnimals = 'cow horse sheep pig chicken';
+
+const farmAnimals = "cow horse sheep pig chicken";
+
+
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(" ");
+
+
+console.log(moo, neigh, baa, oink, cluck );
+
+
+
+const [bessie, , dolly, babe, little] = farmAnimals.split(" ");
+console.log(bessie, dolly, babe, little);
+
+
+const [blackAndWhite, , black, pink] = farmAnimals.split(" ");
+console.log(blackAndWhite, black, pink);
+console.log(blackAndWhite); // "cow"
+console.log(black);         // "sheep"
+console.log(pink);          // "pig"
+
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
+
+
+const [r, o, y, g, b, , v]=colors;
+const [, , , , ,indg]= colors;
 
 const muppet = {
   muppetName: 'Miss Piggy',
@@ -9,6 +34,10 @@ const muppet = {
   job: 'Cast member of The Muppet Show',
   partner: 'Kermit'
 };
+
+
+const{muppetName, color, song, job, partner}=muppet;
+
 
 const nestedMuppet = {
   nestedName: 'Kermit',
@@ -24,6 +53,16 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
+
+
+const { 
+  album: { theMuppetMovie: { song2, song4 } },
+  nestedJob,
+  nestedPartner
+} = nestedMuppet;
+
+
+
 
 // Strings
 
